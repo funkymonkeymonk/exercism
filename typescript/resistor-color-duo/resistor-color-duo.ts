eba08c0f9a9afb23459039f2b7e9e6c9fc327eed
Will Weaver <monkey@buildingbananas.com>
2020-10-31 20:26:22 -0400

    Solving resistor colors

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -2,7 +2,11 @@ export class ResistorColor {
   private colors: string[];
 
   constructor(colors: string[]) {
-    this.colors = colors;
+    if (colors.length < 2) throw `At least two colors need to be present`
+    this.colors = colors.slice(0,2);
   }
-  value = (): number => 0;
+  value = (): number => {
+    const colorVals = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+    return Number(this.colors.map(color => colorVals.indexOf(color)).join(""))
+  };
 }
