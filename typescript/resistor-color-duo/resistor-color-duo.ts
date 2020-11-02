6a2a17bdad69e2b40eab3538f45fb07366e8abdb
Will Weaver <monkey@buildingbananas.com>
2020-11-02 00:33:58 -0500

    Incorporating feedback for resistor color

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,12 +1,26 @@
 export class ResistorColor {
-  private colors: string[];
+    private colors: string[];
 
-  constructor(colors: string[]) {
-    if (colors.length < 2) throw `At least two colors need to be present`
-    this.colors = colors.slice(0,2);
-  }
-  value = (): number => {
-    const colorVals = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
-    return Number(this.colors.map(color => colorVals.indexOf(color)).join(""))
-  };
+    constructor(colors: string[]) {
+        if (colors.length < 2) throw `At least two colors need to be present`
+        this.colors = colors.slice(0, 2);
+    }
+
+    value = (): number => {
+        const colorVals: { [color: string]: number; } = {
+            'black': 0,
+            'brown': 1,
+            'red': 2,
+            'orange': 3,
+            'yellow': 4,
+            'green': 5,
+            'blue': 6,
+            'violet': 7,
+            'grey': 8,
+            'white': 9
+        };
+
+        const reducer = (acc:number , currVal:string, curIndex:number) => acc + colorVals[currVal] * 10 ** curIndex;
+        return this.colors.reverse().reduce(reducer ,0);
+    };
 }
